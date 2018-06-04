c8c94d408592e513c9320d25f0a529f3711e86d7
hitsusorisaku <35912946+hitsusorisaku@users.noreply.github.com>
2018-06-04 16:20:03 -0700

    WIP

--- src/clojure_repl/nrepl.cljs
+++ src/clojure_repl/nrepl.cljs
@@ -1,21 +1,28 @@
 (ns clojure-repl.nrepl
   (:require [cljs.nodejs :as node]
-            [oops.core :refer [oget+ oset!+ ocall]]
+            [oops.core :refer [oget+ oset! oset!+ ocall]]
             [clojure-repl.common :refer [console-log]]
-            [cljs.core.async :as async :refer [chan timeout <!]])
+            [clojure-repl.bencode :refer [decode]]
+            [cljs.core.async :as async :refer [chan closed? <!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def bencode (node/require "bencode"))
 (def net (node/require "net"))
-(def stream (node/require "stream"))
 
 (defn send
   [connection options callback]
-  (let [message (if (nil? (get options "id"))
-                  (assoc options "id" (.-uuid (random-uuid)))
-                  options)
-        js-message (clj->js message)]
-    (.write (.-socket-connection connection) (.encode bencode js-message) "binary")))
+  (let [id (or (get options "id")
+               (.-uuid (random-uuid)))
+        message (assoc options "id" id)
+        js-message (clj->js message)
+        from-channel (chan [])
+        out-channel (chan [])
+        queued-messages []]
+    (swap! (.-from-channel connection) assoc id from-channel)
+    (swap! (.-out-channel connection) assoc id out-channel)
+    (swap! (.-queued-messages connection) assoc id queued-messages)
+    (swap! (.-callbacks connection) assoc id callback)
+    (.write (.-socket-connection connection) (.encode bencode js-message) "binary")
+    out-channel))
 
 (defn eval ""
   ([connection code callback]
@@ -48,8 +55,6 @@
 (defn close [connection]
   (.end (.-socket-connection connection)))
 
-(defrecord SocketMessages [bytes-left chunk-left messages])
-
 ;; Use defmulti/defmethod instead
 (comment (defprotocol ConnectionProtocol
           (send [])
@@ -61,53 +66,51 @@
           (loadFile [])
           (stdin [])))
 
-(defrecord Connection [socket-connection sessions])
+(defrecord Connection [socket-connection
+                       from-channels
+                       out-channels
+                       queued-messages
+                       callbacks])
 
-(defn ^:private consume-messages-with-id [socket-connection id]
-  (let [channel (chan [])]
-    (loop [chunk (.read socket-connection)]
-      (when (some? chunk)
-        (conj channel chunk)
-        (recur (.read socket-connection))))
-    channel))
+(defn ^:private has-done-message? [messages]
+  (and (last messages)
+       (.-status (last messages))
+       (some? #(= % "done") (.-status (last messages)))))
 
-(defn ^:private read-data-2 [socket-connection]
-  (console-log "Reading data...")
-  (let [data (.read socket-connection)]
-    (while (some? data)
-     (console-log "Processing data...")
-     (let [decoded-data (.decode bencode data "utf8")]
-       (console-log "DATA arrived as stream!" decoded-data)))))
+(defn ^:private collect-messages [id queued-messages callback]
+  (loop []
+    (cond
+      (has-done-message? queued-messages) (callback nil queued-messages)
+      (closed? from-channel) (callback "Error: timedout" nil)
+      (recur))))
 
-(defn consume-all-data [data]
-  (let [decoded-data (.decode bencode data "utf8")
-        decode (.-decode bencode)]
-    (console-log "Data: " decoded-data)
+(defn ^:private  consume-all-data
+  "Call recur on loop only when there's no error calling decode.next(). When
+  error happens, it means there isn't enough information to fully decode the
+  data. So we need to cache all the current data we got and wait for the
+  remaining data to become available to be consumed."
+  [connection data]
+  (when-let [decoded-data (decode connection data)]
     (loop []
-     (console-log "Checking more... " (.-length (.-data decode)) (.-position decode))
-     (when (> (.-length (.-data decode)) (.-position decode))
-       (do
-         (console-log "More data: " (.next decode))
-         (recur))))))
+     (console-log "Checking more... " (decoded-all?))
+     (if (decoded-all?)
+       decoded-data
+       (recur)))))
 
-(defn ^:private read-data [socket-connection]
-  (console-log "Reading data...")
-  (loop [data (.read socket-connection)
-         try-counter 0]
-    (if (some? data)
-     (do
-       (console-log "DATA arrived as stream!" data)
-       (consume-all-data data)
-       (recur (.read socket-connection) try-counter))
-     (if (> 50 try-counter)
-       (recur (.read socket-connection) (inc try-counter))
-       (console-log "Tried polling enough...")))))
+(defn ^:private received-all-messages? [connection id]
+  (let [messages (oget!+ (.-queued-messages connection) id)]
+    (has-done-message? messages)))
+
+(defn ^:private read-data [connection data]
+  (console-log "Reading data... " data)
+  (consume-all-data connection data))
+
+(defn clone [connection callback])
+
 
 (defn connect [options]
   (let [socket-connection (.connect net (clj->js options))
-        sessions []
-        connection (Connection. socket-connection sessions)
-        messages []
-        on-readable (partial read-data socket-connection)]
+        connection (Connection. socket-connection (atom {}) (atom {}) (atom {}) (atom {}))
+        on-readable (partial read-data connection)]
     (.on socket-connection "readable" on-readable)
     connection))
