931c884d980fcf8b40a37c8701082971ff1d7022
hitsusorisaku <35912946+hitsusorisaku@users.noreply.github.com>
2018-06-18 11:00:42 -0700

    Basic working nrepl client!

--- src/clojure_repl/nrepl.cljs
+++ src/clojure_repl/nrepl.cljs
@@ -6,7 +6,7 @@
             [clojure-repl.bencode :as bencode :refer [encode decode]]
             [clojure-repl.repl :as repl]
             [cljs.core.async :as async :refer [chan timeout close! <! >! alts!]])
-  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (def net (node/require "net"))
 
@@ -18,22 +18,24 @@
 ;; an elegant mechanism for putting a time limit on concurrent operations.
 (defn send
   [connection message callback]
-  (let [js-message (clj->js message)]
-    (.write (:socket-connection connection) (encode js-message))))
+  (let [id (or (get message "id")
+               (.-uuid (random-uuid)))
+        message-js (clj->js (assoc message "id" id))]
+    (swap! (:queued-messages connection) assoc id [])
+    (swap! (:callbacks connection) assoc id callback)
+    (.write (:socket-connection connection) (encode message-js))))
 
 (defn eval [connection code options callback]
-    (let [id (or (get options "id")
-                 (.-uuid (random-uuid)))]
-      (swap! (:queued-messages connection) assoc id [])
-      (swap! (:callbacks connection) assoc id callback)
-      (send connection (assoc options "op" "eval"
-                                      "code" code
-                                      "id" id)
-                       callback)))
+  (send connection
+        (assoc options "op" "eval"
+                       "code" code)
+        callback))
 
 (defn clone-connection
-  ([connection callback] (send connection {"op" "clone"} callback))
-  ([connection session callback] (send connection {"op" "clone" "session" session} callback)))
+  ([connection callback]
+    (send connection {"op" "clone"} callback))
+  ([connection session callback]
+    (send connection {"op" "clone" "session" session} callback)))
 
 (defn close [connection]
   (.end (:socket-connection connection)))
@@ -61,8 +63,10 @@
        (some #(= % "done") (.-status (last messages)))))
 
 (defn ^:private callback-with-queued-messages [connection id]
-  (when-let [messages (aget @(:queued-messages connection) id)]
-    (when-let [callback (aget @(:callbacks connection) id)]
+  (when-let [messages (get @(:queued-messages connection) id)]
+    (console-log "Callback on " messages)
+    (when-let [callback (get @(:callbacks connection) id)]
+      (console-log "Callback is " callback)
       (when (has-done-message? messages)
         (callback nil messages)))))
 
@@ -70,16 +74,16 @@
   "Keeps reading data from the socket until it's been depleated. All data is
   decoded and put into a channel. The channel is closed when no more data
   can be read from the socket."
-  [socket-connection]
-  (let [message-chan (chan)]
-    (go-loop [chunk (.read socket-connection)]
-      (if (some? chunk)
-        (let [messages (decode chunk)]
-          (when-not (empty? messages)
-            (>! message-chan messages)
-            (recur (.read socket-connection))))
-        (close! message-chan)))
-    message-chan))
+  [socket-connection message-chan]
+  (go-loop [chunk (.read socket-connection)]
+    (console-log "Consuming data..." )
+    (if (some? chunk)
+      (let [messages (decode chunk)]
+        (when-not (empty? messages)
+          (console-log "Decoded data...")
+          (>! message-chan messages)
+          (recur (.read socket-connection))))
+      (close! message-chan))))
 
 (defn ^:private read-data
   "Called when socket dispatches readable event, having data available
@@ -87,16 +91,18 @@
   output-channel after converting them into messages. message-chan will
   eventually takes nil out and breaks out of loop-recur when it's been closed."
   [connection]
-  (console-log "Reading data...")
-  (let [message-chan (consume-all-data (:socket-connection connection))]
+  (console-log "Reading data... " connection)
+  (let [message-chan (chan)]
     (go-loop []
       (when-let [messages (<! message-chan)]
+        (console-log "Messages: " messages)
         (>! (:output-channel connection) messages)
-        (map (fn [{:keys [id] :as message}]
-               (swap! (:queued-messages connection) update id conj message)
-               (callback-with-queued-messages connection id))
-             messages)
-        (recur)))))
+        (doseq [message-js messages]
+          (let [id (.-id message-js)]
+            (swap! (:queued-messages connection) update id conj message-js)
+            (callback-with-queued-messages connection id)))
+        (recur)))
+    (consume-all-data (:socket-connection connection) message-chan)))
 
 ;; TODO: Support having multiple connections to REPL using bencode.
 ;; TODO: Support type ahead by creating a new session to send the code.
@@ -105,60 +111,84 @@
 ;;  1. General REPL's messages, especially for printing the initial description of the REPL -> session-id
 ;;  2. Replies that come back from the message sent to the REPL -> message-id
 
+;; TODO: Handle clone error
+(defn ^:private get-new-session-message
+  [connection]
+  (let [message-chan (chan)]
+    (go
+      (when-let [messages (<! message-chan)]
+        (doseq [message-js messages]
+          (let [id (.-id message-js)]
+            (console-log "Message: " message-js)
+            (swap! (:queued-messages connection) update id conj message-js)
+            (callback-with-queued-messages connection id)))))
+    (consume-all-data (:socket-connection connection) message-chan)))
+
 (defn connect
   [{:keys [host port project-name]} callback]
   (let [socket-connection (net.Socket.)
         connection (assoc connection-template :socket-connection socket-connection
                                               :output-channel (chan)
                                               :queued-messages (atom {})
-                                              :callbacks (atom {}))
-        on-readable (partial read-data connection)]
+                                              :callbacks (atom {}))]
     (.connect socket-connection
               port
               host
               (fn []
-                (clone-connection socket-connection
-                                  (fn [err message]
-                                    (callback connection (oget message ["0" "new-session"]))))
-                (.on socket-connection "readable" on-readable)))
+                (.once socket-connection "readable" #(get-new-session-message connection))
+                (clone-connection connection
+                                  (fn [err messages]
+                                    (.on socket-connection "readable" #(read-data connection))
+                                    (callback connection (oget (get messages 0) "new-session"))))))
     connection))
 
 
 ;; ---------------------------------------------------
 ;; Connection should be enstablished like this:
+(defn append-to-output-editor [project-name output & {:keys [add-newline?] :or {add-newline? true}}]
+  (console-log "OUTPUT: " project-name output))
+
 (defn send-to-nrepl [project-name code & [namespace]]
   (let [wrapped-code (repl/wrap-to-catch-exception code)
         {:keys [connection session current-ns]} (get @repls project-name)
         options {"session" session
                  "ns" (or namespace current-ns)}]
-    (eval connection wrapped-code options (fn [errors messages]
-                                            (when (repl/namespace-not-found? (last messages))
-                                              (console-log "Resending code to the current namespace...")
-                                              (send-to-nrepl project-name code))))))
+    (eval connection
+          wrapped-code
+          options
+          (fn [errors messages]
+            (when (repl/namespace-not-found? (last messages))
+              (console-log "Resending code to the current namespace...")
+              (send-to-nrepl project-name code))))))
 
 (defn output-namespace [project-name]
-  (repl/append-to-output-editor project-name (str (get-in @repls [project-name :current-ns]) "=> ") :add-newline? false))
+  (append-to-output-editor project-name (str (get-in @repls [project-name :current-ns]) "=> ") :add-newline? false))
 
-(defn output-messages [project-name message]
-  (when (.-ns message)
-    (swap! repls update project-name #(assoc % :current-ns (.-ns message))))
-  (if (.-out message)
-    (repl/append-to-output-editor project-name (string/trim (.-out message)))
-    (when (.-value message)
-      (repl/append-to-output-editor project-name (.-value message)))))
+(defn output-message [project-name message-js]
+  (when (.-ns message-js)
+    (swap! repls update project-name #(assoc % :current-ns (.-ns message-js))))
+  (if (.-out message-js)
+    (append-to-output-editor project-name (string/trim (.-out message-js)))
+    (when (.-value message-js)
+      (append-to-output-editor project-name (.-value message-js)))))
 
 (defn filter-by-session [session messages]
   (filter #(= (.-session %) session) messages))
 
 (defn handle-messages [project-name connection]
-  (let [session (get-in @repls [project-name :session])]
+  (console-log "Handler called...")
+  (let [output-chan (:output-channel connection)]
     (go-loop []
-      (let [messages (filter-by-session session (<! (:output-channel connection)))]
-        (when (not-any? repl/namespace-not-found? messages)
-          (map #(output-messages project-name %) messages)
-          (if (has-done-message? messages)
-            (output-namespace project-name)
-            (recur)))))))
+      (when-let [messages (<! output-chan)]
+        (console-log "Handleing messages..." messages)
+        (let [session (get-in @repls [project-name :session])
+              filtered-messages (filter-by-session session messages)]
+          (when (not-any? repl/namespace-not-found? filtered-messages)
+            (doseq [message-js filtered-messages]
+              (output-message project-name message-js))
+            (when (has-done-message? filtered-messages)
+              (output-namespace project-name))))
+        (recur)))))
 
 (defn update-most-recent-repl [project-name]
   (when (nil? (get @state :most-recent-repl-project-name))
@@ -172,15 +202,17 @@
   (when (get @repls project-name)
     (close-connection project-name))
   (connect {:project-name project-name
-                             :host host
-                             :port port}
-                             (fn [connection session]
-                               (when session
-                                 (swap! repls update
-                                        project-name
-                                        #(assoc repl-state
-                                           :repl-type :repl-type/nrepl
-                                           :connection connection
-                                           :session session))
-                                 (update-most-recent-repl project-name)
-                                 (handle-messages project-name connection)))))
+            :host host
+            :port port}
+           (fn [connection session]
+             (console-log "Connection succeeded!! " connection)
+             (when session
+               (console-log "Session is here..." session)
+               (swap! repls update
+                     project-name
+                     #(assoc repl-state
+                       :repl-type :repl-type/nrepl
+                       :connection connection
+                       :session session))
+               (update-most-recent-repl project-name)
+               (handle-messages project-name connection)))))
