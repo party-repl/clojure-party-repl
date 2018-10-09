4ee7ef544066348bce8cf26458e85eea6479fc59
hitsusorisaku <35912946+hitsusorisaku@users.noreply.github.com>
2018-10-09 15:08:09 -0700

    Added more readers

--- src/clojure_party_repl/unrepl.cljs
+++ src/clojure_party_repl/unrepl.cljs
@@ -14,10 +14,33 @@
 (def net (node/require "net"))
 (def fs (node/require "fs"))
 
-(fs.readFile (str  "..../resources/unrepl/blob.clj")
+;; TODO: Make it readable from the Atom package
+(fs.readFile (str  "/clojure-party-repl/resources/unrepl/blob.clj")
    (fn [err data]
      (when err (console-log err (ocall process "cwd")))
      (def unrepl-blob data)))
+
+(def ^:private ellipsis "...")
+(def ^:private elision-key :get)
+
+(defn ^:private has-elision?
+  "Elision is expressed as either `#unrepl/... nil` or
+  `#unrepl/... {:get continuation-fn}` at the end of a collection. Elision allows
+  us to have control over how much and when we want to show the details.
+
+  There are a few places it could appear:
+    1. Lazy sequence
+    2. Long string
+    3. Stacktraces
+
+  Elision can either be at the end of a collection"
+  [coll]
+  (if (and (coll? coll)
+           (or (and (satisfies? PersistentArrayMap coll)
+                    (contains? coll elision-key))
+               (last coll)))
+    true
+    false))
 
 (defmulti handle-unrepl-tuple
   (fn [project-name tuple]
@@ -25,10 +48,6 @@
       ::not-a-tuple
       (let [[tag payload group-id] tuple]
         tag))))
-
-;; NOTE: Other tags to support are
-;;        [:exception {:ex exception :phase {}} 0]
-;;        [:read {:from [line col] :to [line col] :offset N :len N} 1]
 
 (defmethod handle-unrepl-tuple ::not-a-tuple [project-name thing]
   (repl/append-to-output-editor (pr-str thing) :add-newline? true))
@@ -51,8 +70,27 @@
 (defmethod handle-unrepl-tuple :err [project-name [tag payload group-id]]
   (repl/append-to-output-editor project-name payload :add-newline? false))
 
+;; The exception has a shape of:
+;; {:ex {:cause String
+;;       :via [{:type Exception
+;;              :message String
+;;              :at [StackTrace, String, at, {...get}]}, ...]
+;;       :trace [[], [], ..., {...get}]}}
+;;  :phase :eval/}
 (defmethod handle-unrepl-tuple :exception [project-name [tag payload :as tuple]]
-  (console-log "Noop unrepl tuple:" (pr-str tuple)))
+  (let [{:keys [ex phase]} payload
+        {:keys [cause via trace]} ex
+        [{:keys [type _ _]} _] via]
+    (repl/append-to-output-editor project-name
+                                  (string/join " " [type cause (-> trace
+                                                                   (get 0 [])
+                                                                   (get 2 ""))])
+                                  :add-newline? true)
+    (doseq [[_ _ at] (vec (butlast (next trace)))] ;; TODO: The last item is elisions map. Show "..."?
+      (when-not (coll? at)
+        (repl/append-to-output-editor project-name
+                                      (str \tab at)
+                                      :add-newline? true)))))
 
 (defmethod handle-unrepl-tuple :prompt [project-name [tag payload]]
   (repl/append-to-output-editor project-name
@@ -65,25 +103,66 @@
 
 (defmethod handle-unrepl-tuple :unrepl/hello [project-name [tag payload]]
   (let [{:keys [session actions]} payload]
-    (console-log "Session is: " payload)))
+    (console-log "Session is: " session)))
+
+(defn read-string [string]
+  (cond
+    (string? string) (identity string)
+    (coll? string) (let [[prefix elisions] string]
+                     (str prefix " " ellipsis))))
+
+(defn read-ratio [[a b]]
+  (symbol (str a "/" b)))
+
+(defn read-object [[class id representation :as object]]
+  (identity object))
+
+(defn read-elision
+  "Elision can either be nil or a map with the continuation function associated
+  to the :get key."
+  [elisions]
+  (if (nil? elisions)
+    ellipsis
+    (identity elisions)))
+
+(defn read-lazy-error
+  "An exception #unrepl/lazy-error is inlined in a sequence when
+  realization of a lazy sequence throws.
+
+  For example, as stated in unrepl spec:
+  (map #(/ %) (iterate dec 3))
+  will return
+  (#unrepl/ratio [1 3] #unrepl/ratio [1 2] 1 #unrepl/lazy-error #error {:cause \"Divide by zero\", :via [{:type #unrepl.java/class java.lang.ArithmeticException, :message \"Divide by zero\", :at #unrepl/object [#unrepl.java/class java.lang.StackTraceElement \"0x272298a\" \"clojure.lang.Numbers.divide(Numbers.java:158)\"]}], :trace [#unrepl/... nil]})
+
+  TODO: Also show a stacktrace below the result?"
+  [{:keys [cause via trace]}]
+  (let [[{:keys [type message _]} _] via]
+    (apply console-log cause)
+    (string/join " " [type message (-> trace
+                                       (get 0 [])
+                                       (get 2 ""))])))
 
 (defn ^:private read-unrepl-stream
-  "Read and process all the unrepl tuples in the given data string."
+  "Read and process all the unrepl tuples in the given data string.
+
+  For each supported tagged literal, we need to provide a reader function. If
+  not provided, \"No reader function for tag error.\" will be thrown."
   [project-name data]
   (let [reader (string-push-back-reader (str data))]
     (loop []
       (let [msg (read {:eof ::eof
-                       :readers {'unrepl/param identity
+                       :readers {'clojure/var identity
+                                 'unrepl/param identity
                                  'unrepl/ns identity
-                                 'unrepl/string identity
-                                 'unrepl/ratio identity
+                                 'unrepl/string read-string
+                                 'unrepl/ratio read-ratio
                                  'unrepl/meta identity
                                  'unrepl/pattern identity
-                                 'unrepl/object identity
+                                 'unrepl/object read-object
+                                 'unrepl/mime identity
                                  'unrepl.java/class identity
-                                 'unrepl/... #(if (nil? %) "nil" (identity %))
-                                 'unrepl/lazy-error identity
-                                 'clojure/var identity
+                                 'unrepl/... read-elision
+                                 'unrepl/lazy-error read-lazy-error
                                  'error identity}}
                       reader)]
         (console-log (prn-str msg))
